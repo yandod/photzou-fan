1878e38f3b1df09e5428d537515db9368235d094
yandod <yandod@gmail.com>
2011-08-06 00:48:10 -0400

    playing with photozou feed.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  layout nil
+  def index
+    require 'rss'
+    require 'net/http'
+    require 'uri'
+    rss_source = Net::HTTP.get URI.parse('http://photozou.jp/feed/photo_pickup.xml')
+    @rss = RSS::Parser.parse(rss_source, true)
+    @rss.items.each {|item|
+      item.description[/_thumbnail/] = ''
+      item.description[/width="120" height="120"/] = ''
+    }
+  end
+
+end
