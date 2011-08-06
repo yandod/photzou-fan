f30c55618bfa4b39f2e349918bf5b75c0e556bc3
yandod <yandod@gmail.com>
2011-08-06 00:53:16 -0400

    change title.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
     require 'uri'
     rss_source = Net::HTTP.get URI.parse('http://photozou.jp/feed/photo_pickup.xml')
     @rss = RSS::Parser.parse(rss_source, true)
+    @rss.title << '(Large)'
     @rss.items.each {|item|
       item.description[/_thumbnail/] = ''
       item.description[/width="120" height="120"/] = ''
