224cfdef9ef9c9c7e2213fcf322f42672b545f8a
yandod <yandod@gmail.com>
2011-08-05 22:22:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotzouFan::Application.config.secret_token = '27357ed9771299af1d16fb1652cac7fbe3b6ccd3e3eeb0926f721eba845312022e3af832653a6a92087327832699ff77d32220b17b985d9751b569fade3d4e8b'
